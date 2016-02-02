49b12aee98196f0918f95707552f909fb02bd54c
Andreas Gohr <andi@splitbrain.org>
2016-02-02 13:08:33 +0100

    Adjust method signatures to match parent

--- action.php
+++ action.php
@@ -17,7 +17,7 @@ class action_plugin_eventum extends DokuWiki_Action_Plugin {
     /**
      * plugin should use this method to register its handlers with the dokuwiki's event controller
      */
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
       $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button');
     }
 
