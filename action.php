60bd12cfcda951250b2642e88bd11d2dc4b44925
Elan Ruusamäe <glen@delfi.ee>
2019-02-13 19:36:38 +0200

    WIP

--- action.php
+++ action.php
@@ -24,6 +24,7 @@ class action_plugin_eventum extends DokuWiki_Action_Plugin
     public function register(Doku_Event_Handler $controller)
     {
         $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insertToolbarButton');
+        $controller->register_hook('IO_WIKIPAGE_WRITE', 'AFTER', $this, 'addRemoteIssueLinks');
     }
 
     /**
@@ -39,6 +40,59 @@ class action_plugin_eventum extends DokuWiki_Action_Plugin
             'close' => ']]',
             'key' => 'e',
         );
+    }
+
+    private $alreadyTriggered;
+
+    /**
+     * Add remote issue links
+     *
+     * @param Doku_Event $event
+     * @param mixed $param
+     */
+    public function addRemoteIssueLinks(Doku_Event $event, $param)
+    {
+        if ($this->alreadyTriggered) {
+            return;
+        }
+
+        global $ID, $INFO, $conf;
+
+        // Look for issue keys
+        if (preg_match_all('/[A-Z]+?-[0-9]+/', $event->data[0][1], $keys)) {
+            // Keys found, prepare data for the remote issue link
+            $keys = array_unique($keys[0]);
+            $url = wl($ID, '', true);
+            // MD5 hash is used because the global id max length is 255 characters. An effective page URL might be longer.
+            $globalId = md5($url);
+            $applicationName = $conf['title'];
+            $applicationType = 'org.dokuwiki';
+            $title = $applicationName . ' - ' . (empty($INFO['meta']['title']) ? $event->data[2] : $INFO['meta']['title']);
+            $relationship = $this->getConf('url_relationship');
+            $favicon = tpl_getMediaFile(array(':wiki:favicon.ico', ':favicon.ico', 'images/favicon.ico'), true);
+
+            foreach ($keys as $key) {
+                $data = array(
+                    'globalId' => $globalId,
+                    'application' => array(
+                        'type' => $applicationType,
+                        'name' => $applicationName,
+                    ),
+                    'relationship' => $relationship,
+                    'object' => array(
+                        'url' => $url,
+                        'title' => $title,
+                        'icon' => array(
+                            'url16x16' => $favicon,
+                        ),
+                    ),
+                );
+
+                $this->executeRequest("issue/{$key}/remotelink", 'POST', $data);
+            }
+
+            $this->alreadyTriggered = true;
+        }
     }
 }
 
