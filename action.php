d84e15cd818a225f7000a7c8eae8615a43ca13bb
Elan Ruusamäe <glen@delfi.ee>
2019-02-13 16:26:03 +0200

    codestyle: set methods visibility

--- action.php
+++ action.php
@@ -17,14 +17,14 @@ class action_plugin_eventum extends DokuWiki_Action_Plugin {
     /**
      * plugin should use this method to register its handlers with the dokuwiki's event controller
      */
-    function register(Doku_Event_Handler $controller) {
-      $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button');
+    public function register(Doku_Event_Handler $controller) {
+      $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insertToolbarButton');
     }
 
     /**
      * Inserts a toolbar button
      */
-    function insert_button(&$event) {
+    public function insertToolbarButton(Doku_Event $event) {
         $event->data[] = array(
             'type' => 'format',
             'title' => $this->getLang('btn_issue'),
